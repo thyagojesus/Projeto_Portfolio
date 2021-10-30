5930aa94b92361fc964a1e5d74651065786ef262
Thyago Jesus <thyagojesus.a@gmail.com>
2021-10-30 19:10:13 -0300

    contact section completed

--- together.js
+++ together.js
@@ -32,10 +32,26 @@ window.addEventListener('scroll', function() {
 })
 
 const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
+    slidesPerView: 3,
     pagination: {
         el: '.swiper-pagination'
     },
     mousewheel: true,
     keyboard: true
 })
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials
+    #contact .text, #contact .links
+    `, { interval: 100 }
+)
