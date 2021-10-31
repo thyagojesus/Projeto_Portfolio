f7833ded693af21f176032e25be5a66015185b19
Thyago Jesus <thyagojesus.a@gmail.com>
2021-10-30 22:55:10 -0300

    finished mobile version

--- together.js
+++ together.js
@@ -18,10 +18,10 @@ for (const link of links) {
 }
 
 /* mudar o header da página quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
     if (window.scrollY >= navHeight) {
         // scroll é maior que a altura do header
         header.classList.add('scroll')
@@ -29,8 +29,9 @@ window.addEventListener('scroll', function() {
         // menor que a altura do header
         header.classList.remove('scroll')
     }
-})
+}
 
+/* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     pagination: {
@@ -40,6 +41,7 @@ const swiper = new Swiper('.swiper-container', {
     keyboard: true
 })
 
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
     origin: 'top',
     distance: '30px',
@@ -49,9 +51,27 @@ const scrollReveal = ScrollReveal({
 
 scrollReveal.reveal(
     `#home .image, #home .text,
-    #about .image, #about .text,
-    #services header, #services .card,
-    #testimonials header, #testimonials .testimonials
-    #contact .text, #contact .links
-    `, { interval: 100 }
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `, { interval: 100 }
 )
+
+/* Botão voltar para o topo */
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
+})
