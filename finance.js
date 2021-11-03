41cee8b31f1b4af9007ac7892f4a438c7a647139
Thyago Jesus <thyagojesus.a@gmail.com>
2021-11-02 21:02:59 -0300

    modal created

---
+++ finance.js
@@ -0,0 +1,9 @@
+const Modal = {
+    open() {
+        document.querySelector('.modal-overlay').classList.add('active')
+    },
+    clouse() {
+        document.querySelector('.modal-overlay').classList.remove('active')
+    }
+}
+alert('Em construção- ( Click em OK )')
